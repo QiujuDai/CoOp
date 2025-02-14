7c996e6df9dbfe114bd75b4d9675b35d51530608
QiujuDai <qdai43@uwo.ca>
2025-02-13 23:58:23 -0500

    fix read_data params

--- datasets/mushrooms.py
+++ datasets/mushrooms.py
@@ -21,10 +21,10 @@ class Mushrooms(DatasetBase):
         self.img_dir = os.path.join(self.dataset_dir, "data")
 
         if os.path.exists(self.split_path):
-            train, val, test = self.read_split(self.split_path, self.img_dir)
+            train, val, test = self.read_split(self.split_path, self.dataset_dir)
         else:
-            trainval = self.read_data(split_file="trainval.txt", img_dir=self.dataset_dir)
-            test = self.read_data(split_file="test.txt", img_dir=self.dataset_dir)
+            trainval = self.read_data(split_file="trainval.txt", img_dir=self.img_dir)
+            test = self.read_data(split_file="test.txt", img_dir=self.img_dir)
             train, val = self.split_trainval(trainval)
             self.save_split(train, val, test, self.split_path)
 
