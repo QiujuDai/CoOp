0a592459068ceeb640cad330e88ad789e355790f
QiujuDai <qdai43@uwo.ca>
2025-02-14 19:26:41 -0500

    add dbg print of mushroom data

--- datasets/mushrooms.py
+++ datasets/mushrooms.py
@@ -40,17 +40,14 @@ class Mushrooms(DatasetBase):
                     train, val = data["train"], data["val"]
             else:
 
-                for item in train:
-                    print(f"333 impath:{item.impath}")
-                    # impath2 = os.path.join(img_dir, impath)
-                    # # print(f"222 impath:{impath2}, img_dir:{img_dir}")
-                    
-                    # item = Datum(impath=impath2, label=int(label), classname=classname)
-                    # out.append(item)
-                    # return out
+                for item in val:
+                    print(f"444 impath:{item.impath}")
 
                 train = self.generate_fewshot_dataset(train, num_shots=num_shots)
                 val = self.generate_fewshot_dataset(val, num_shots=min(num_shots, 4))
+
+                for item in val:
+                    print(f"555 impath:{item.impath}")
                 data = {"train": train, "val": val}
                 print(f"Saving preprocessed few-shot data to {preprocessed}")
                 with open(preprocessed, "wb") as file:
